abdc548e5d8b2a28bcc29c24c0e3d4e6f1295909
Pungpeee <60164550+Pungpeee@users.noreply.github.com>
2020-04-06 22:07:15 +0700

    มีpricePerRoomแล้ว

---
+++ src/Hotel/PricePerRoom.java
@@ -0,0 +1,9 @@
+package Hotel;
+
+public interface PricePerRoom {
+
+    public static final int PRICE_SingleRoom = 700;
+    public static final int PRICE_DoubleRoom = 900;
+    public static final int PRICE_QuadRoom = 1500;
+    public static final int PRICE_SuiteRoom = 2500;
+}
--- src/Room/TypeOfRoom.java
+++ src/Room/TypeOfRoom.java
@@ -1,27 +1,35 @@
 
 package Room;
 
-public abstract class TypeOfRoom {
-    private int rooms;
+import Hotel.PricePerRoom;
+import Hotel.Status;
+
+public abstract class TypeOfRoom implements PricePerRoom {
+    
     private String nameOfRooms;
+    private Status statusRoom;
+    
 
-    public TypeOfRoom(int rooms, String nameOfRooms) {
-        this.rooms = rooms;
+    public TypeOfRoom(String nameOfRooms, Status statusRoom) {
         this.nameOfRooms = nameOfRooms;
-    }
-
-    public int getRooms() {
-        return rooms;
+        this.statusRoom = statusRoom;
+        
     }
 
     public String getNameOfRooms() {
         return nameOfRooms;
     }
 
+    public Status getStatusRoom() {
+        return statusRoom;
+    }
+
     @Override
     public String toString() {
-        return "TypeOfRoom{" + "rooms=" + rooms + ", nameOfRooms=" + nameOfRooms + '}';
+        return "TypeOfRoom{" + "nameOfRooms=" + nameOfRooms + ", statusRoom=" + statusRoom + '}';
     }
+
+    
     
     public abstract int numberOfRooms();
     public abstract int emptyRoom();
