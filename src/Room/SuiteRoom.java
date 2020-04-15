baea470b1b90b5980e3ad2d43efd4c0ac6cd6d6d
Nurse01 <60164787+Nurse01@users.noreply.github.com>
2020-04-15 23:38:20 +0700

    nurse

--- src/Room/SuiteRoom.java
+++ src/Room/SuiteRoom.java
@@ -14,12 +14,17 @@ public class SuiteRoom extends Hotel implements PricePerRoom{
 
     @Override
     public int numberOfRooms() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        int[] rooms = new int[]{401, 402, 403, 404, 405, 406, 407, 408, 409, 410};
+        for (int i = 0; i < rooms.length; i++) {
+            System.out.println("names[" + i + "] = " + rooms[i]);
+        }
+        return rooms.length;
     }
+    
 
     @Override
     public int emptyRoom() {
-        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
+        
     }
 
     @Override
