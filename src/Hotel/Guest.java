b8b0dcb858d4aceac4f1cf2efb9a9c40c59ecbe5
Nurse01 <60164787+Nurse01@users.noreply.github.com>
2020-04-20 00:21:47 +0700

    testgest

--- src/Hotel/Guest.java
+++ src/Hotel/Guest.java
@@ -1,15 +1,22 @@
 package Hotel;
 
+import java.util.Scanner;
+
 
 public class Guest {
     private String firstname;
     private String surname;
     private int rooms;
+    Scanner IP_name = new Scanner(System.in);
 
     public Guest(String firstname, String surname, int rooms) {
         this.firstname = firstname;
         this.surname = surname;
         this.rooms = rooms;
+    }
+
+    public Guest() {
+        
     }
 
     public String getFirstname() {
@@ -24,12 +31,15 @@ public class Guest {
         return rooms;
     }
 
-    public void setFirstname(String firstname) {
-        this.firstname = firstname;
+    public void setFirstname() {
+        System.out.println("Firstname : ");
+        firstname = IP_name.nextLine();
     }
 
-    public void setSurname(String surname) {
-        this.surname = surname;
+    public void setSurname() {
+        System.out.println("Surname : ");
+        surname = IP_name.nextLine();
+        
     }
 
     public void setRooms(int rooms) {
--- src/Test.java
+++ src/Test.java
@@ -1,41 +1,46 @@
 
+import Hotel.Guest;
 import Room.BookedRoom;
 import java.util.Scanner;
 
-
 public class Test {
+
     private static int number;
-    
+
     public static void main(String[] args) {
         BookedRoom bk = new BookedRoom();
-        
+        Guest g = new Guest();
         Scanner num = new Scanner(System.in);
-        
-        do{
+
+        do {
             System.out.println("--------------------<< Main Menu >>----------------------");
             System.out.println("1.Booked Room");
             System.out.println("2.");
             System.out.println("3. ");
             System.out.println("Enter Your Menu(1-3) : ");
-            
+
             number = num.nextInt();
-            if(number == 1){
+            if (number == 1) {
                 System.out.println("//////Booked Room\\\\\\");
-                bk.setName();
+                g.setFirstname();
+                g.setSurname();
+                System.out.println("Name : " + g.getFirstname() + g.getSurname());
+
                 bk.setTypeOfRoom();
                 bk.setTotalOfRoom();
-                
-            } else if (number == 2){
+
+            } else if (number == 2) {
                 System.out.println("");
                 //รอชื่อ method
-                
-            } else if (number == 3){
+
+            } else if (number == 3) {
                 System.out.println("");
                 //รอชื่อ method
-                
-            }else 
+
+            } else {
                 System.out.println("Please input 1-3");
-        }while( number != 3);
-        
+            }
+        } while (number != 3);
+
     }
 }
